b7c6a4c074b744a8ff8cbfa207abe63d05c4fa4a
goreleaserbot <goreleaser@carlosbecker.com>
2017-11-17 00:31:25 +0100

    zeus version v0.8.3

--- zeus.rb
+++ zeus.rb
@@ -1,11 +1,15 @@
 class Zeus < Formula
   desc "An Electrifying Build System"
   homepage "https://github.com/dreadl0ck/zeus"
-  url "https://github.com/dreadl0ck/zeus/releases/download/v0.8.2/zeus_0.8.2_darwin_amd64.tar.gz"
-  version "0.8.2"
-  sha256 "dea6fe3c62af8823c05088d9c01e367b9752f63ff8334e3652ca7b67d578f32c"
+  url "https://github.com/dreadl0ck/zeus/releases/download/v0.8.3/zeus_0.8.3_darwin_amd64.tar.gz"
+  version "0.8.3"
+  sha256 "6f4a9f65f9432d4847fb53a582611190b21380a81480f5ec683419a7f18c9eb7"
 
   def install
     bin.install "zeus"
+  end
+
+  test do
+    
   end
 end
