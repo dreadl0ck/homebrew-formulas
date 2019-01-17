1e3ee389ba1b5a76aad74a77676c4b067a945e5b
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-17 23:46:58 +0100

    Brew formula update for netcap version v0.3.9

--- netcap.rb
+++ netcap.rb
@@ -1,11 +1,14 @@
 class Netcap < Formula
   desc "A framework for secure and scalable network traffic analysis"
   homepage "https://github.com/dreadl0ck/netcap"
-  url "https://github.com/dreadl0ck/netcap/releases/download/v0.3.8/netcap_0.3.8_darwin_amd64.tar.gz"
-  version "0.3.8"
-  sha256 "075ad68b81b48dc8331096e6f31ffcab251ce733d1a090387d0540fb129d862a"
+  url "https://github.com/dreadl0ck/netcap/releases/download/v0.3.9/netcap_0.3.9_darwin_amd64.tar.gz"
+  version "0.3.9"
+  sha256 "2d53fc6825c7a2e262f6f76332a958eb854455c924310d46ba6d824047445cfb"
 
   def install
     bin.install "netcap"
+    bin.install "netlabel"
+    bin.install "netcap-server"
+    bin.install "netcap-sensor"
   end
 end
