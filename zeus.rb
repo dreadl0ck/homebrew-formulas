4f5a174cf05bb6602ab7b8a18d822b40b958ee18
goreleaserbot <goreleaser@carlosbecker.com>
2021-01-27 11:28:46 +0100

    Brew formula update for zeus version v0.8.8

--- zeus.rb
+++ zeus.rb
@@ -2,16 +2,16 @@
 class Zeus < Formula
   desc "An Electrifying Build System"
   homepage "https://github.com/dreadl0ck/zeus"
-  version "0.8.7"
+  version "0.8.8"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/dreadl0ck/zeus/releases/download/v0.8.7/zeus_0.8.7_darwin_amd64.tar.gz"
-    sha256 "764fc498d63a8fcaf041c14ec372dfb6dfb25631a446bc9e0b2507390ed2f9ef"
+    url "https://github.com/dreadl0ck/zeus/releases/download/v0.8.8/zeus_0.8.8_darwin_amd64.tar.gz"
+    sha256 "89f2c2ea204ccaa7212f59508d8712271547033aa6d57094ed6c74260fe545cc"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/dreadl0ck/zeus/releases/download/v0.8.7/zeus_0.8.7_linux_amd64.tar.gz"
-    sha256 "ed8195d1a750a013dba2ce5f3e48dcef50883feb18e911d11bbe2c35e062e3e7"
+    url "https://github.com/dreadl0ck/zeus/releases/download/v0.8.8/zeus_0.8.8_linux_amd64.tar.gz"
+    sha256 "49dc7c1ea0e621665c42613e7f58d22fb44c3a905a1ebc774902d15ea8c29f0c"
   end
 
   def install
