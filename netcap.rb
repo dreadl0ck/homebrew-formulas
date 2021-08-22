ccd0c9aefac62b3d1f455adceaf2ed950f610abe
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-22 16:16:23 +0200

    Brew formula update for netcap version v0.6.4

--- netcap.rb
+++ netcap.rb
@@ -5,14 +5,14 @@
 class Netcap < Formula
   desc "A framework for secure and scalable network traffic analysis"
   homepage "https://github.com/dreadl0ck/netcap"
-  version "0.6.3"
+  version "0.6.4"
   bottle :unneeded
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/dreadl0ck/netcap/releases/download/v0.6.3/netcap_0.6.3_darwin_amd64.tar.gz"
-      sha256 "c3612e0b8615d5098621ea7ed6fda2569283a04452d44fe97845456dd4af9f0c"
+      url "https://github.com/dreadl0ck/netcap/releases/download/v0.6.4/netcap_0.6.4_darwin_amd64.tar.gz"
+      sha256 "a226ea43be844e4442889dbf7f6326caef542940e9587bb6a9d3ecde3795162e"
     end
   end
 
